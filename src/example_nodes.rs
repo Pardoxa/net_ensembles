afb930f24f9fe485295beefaba762187c162e35d
Yannick Feld <yannick.feld@uol.de>
2020-03-30 17:53:36 +0200

    move node

---
+++ src/example_nodes.rs
@@ -0,0 +1,44 @@
+//! Trait needed for ER graphs etc.
+use crate::traits::*;
+
+/// minimal example for a node
+#[derive(Clone)]
+pub struct TestNode {}
+
+
+impl Node for TestNode {
+    fn new_from_index(_: u32) -> Self {
+        TestNode { }
+    }
+}
+
+/// Use this, if you do not need to store extra information
+#[derive(Clone, Debug)]
+pub struct EmptyNode {}
+
+impl Node for EmptyNode {
+    fn new_from_index(_: u32) -> Self {
+        EmptyNode { }
+    }
+
+    fn make_string(&self) -> Option<String> {
+        Some("".to_string())
+    }
+
+
+    fn parse_str(_to_parse: &str) -> Option<(&str, Self)>
+        where Self: Sized
+    {
+        Some((_to_parse, EmptyNode{ }))
+    }
+}
+
+#[cfg(test)]
+mod testing {
+    use super::*;
+
+    #[test]
+    fn t1() {
+        TestNode::new_from_index(1);
+    }
+}
